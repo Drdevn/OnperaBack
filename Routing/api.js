f723c3ab1490ff4b5d15c167a3b54174737dfec4
drdevn <devnnick@gmail.com>
2018-07-27 16:02:45 +0300

    first post api method

--- Routing/api.js
+++ Routing/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const User = require('../models/user');
 const db = "mongodb://drdevn:asd12345@ds161316.mlab.com:61316/onpback";
 
 mongoose.connect(db, err => {
@@ -14,6 +15,20 @@ mongoose.connect(db, err => {
 
 router.get('/', (req, res) => {
   res.send('From API')
+});
+
+router.post('/adduser', (req, res) => {
+  console.log(req);
+  let userdata = req.body;
+  let user = new User(userdata);
+  user.save((error, newuser) => {
+    if (error) {
+      console.log(error)
+    } else {
+      res.status(200).send(newuser)
+
+    }
+  })
 });
 
 module.exports = router;
