c41e91c142d274b5255328102e195e2c93c01bf2
drdevn <devnnick@gmail.com>
2018-08-16 10:44:00 +0300

    Schema validations

--- Models/page.js
+++ Models/page.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const pageSchema = new Schema({
 
-  name: String,
-  level: String,
+  name: {String, required: true},
+  level: {String, required: true},
   isActive: Boolean,
   childOf: String,
   hasChild: [String],
-  pageContent: [{text: String, position:String, isActive: String }]
+  pageContent: [{text: String, position:String, isActive: String, required: true }]
 
 });
 module.exports = mongoose.model('page', pageSchema, 'pages');
