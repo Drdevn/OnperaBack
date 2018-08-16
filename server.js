e87edf9d38e120b8966c76aaa53410390f723449
drdevn <devnnick@gmail.com>
2018-08-16 11:18:50 +0300

    helmet defence

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const cors = require('cors');
 
 const PORT = 3000;
@@ -11,6 +12,7 @@ const payments = require('./Routing/payments');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(helmet());
 
 app.use('/payments', payments);
 app.use('/api', api);
